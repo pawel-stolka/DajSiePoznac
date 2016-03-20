bb26314a0c6b2cfd20e199194cff5389562970a9
Pablo Ciento <pablo.ciento@gmail.com>
2016-03-20 13:11:45 +0100

    simple enough

--- buenCamino/buenCamino/Scripts/BasicFunctions.js
+++ buenCamino/buenCamino/Scripts/BasicFunctions.js
@@ -7,21 +7,15 @@ $(function getJsonFromDb() {
 
     FillArray();
 
-    $("#btnJson").click(function () {
-
-        //TryToInit();
-    });
-
+    CheckGeoDataIsFilled();
+    
     $("#showJson").click(function () {
         CheckGeoDataIsFilled();
-
     });
 
     $("#JsonAlert").click(function () {
         $("#jsonPlace").html(geoData.length);
     });
-
-    (GetTableRowIndex());
 
 });
 
@@ -33,6 +27,63 @@ function FillArray() {
             geoData.push(value);
         });
     });
-
-    //TryToInit();
 }
+
+function CheckGeoDataIsFilled() {
+    if (geoData.length > 0) {
+        $("#jsonPlace").html("Pushed to array");
+        alert("geoData: " + geoData[0].Latitude + ", " + geoData[0].Longitude);
+        InitMap(geoData[0].Latitude, geoData[0].Longitude);
+    }
+    else {
+        $("#jsonPlace").html("Not yet...");
+    }
+}
+
+function InitMap(startLat, startLon) {
+    google.maps.visualRefresh = true;
+    var StartCenter = new google.maps.LatLng(startLat, startLon);
+    var mapOptions = {
+        zoom: _zoom,
+        center: StartCenter,
+        mapTypeId: google.maps.MapTypeId.G_NORMAL_MAP
+    };
+
+    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+    $.each(geoData, function (i, item) {
+        var marker = new google.maps.Marker({
+            'position': new google.maps.LatLng(item.Latitude, item.Longitude),
+            'map': map,
+            'title': item.PlaceName
+        });
+        var point = new google.maps.LatLng(item.Latitude, item.Longitude);
+        var lineStyle = '#ff0000';
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png')
+
+    });
+
+    map.addListener('mousemove', function (e) {
+        _zoom = 5;
+        var myLatLng = e.latLng;
+        var lat = myLatLng.lat();
+        var lng = myLatLng.lng();
+        $("#moveLat").html(lat);
+        $("#moveLon").html(lng);
+    });
+
+    map.addListener('click', function (event) {
+        var myLatLng = event.latLng;
+        var lat = myLatLng.lat();
+        var lng = myLatLng.lng();
+        $("#posLat").html(lat);
+        $("#posLon").html(lng);
+        //clearing
+        $("#moveLat").html("");
+        $("#moveLon").html("");
+        localStorage.setItem("_tempLat", lat);//.replace(".",","));
+        localStorage.setItem("_tempLon", lng);
+        alert(localStorage.getItem("_tempLat") + " " + localStorage.getItem("_tempLon"));
+    })
+}
+
